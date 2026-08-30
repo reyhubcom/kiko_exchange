632cb6f2d3a70790606e05c4d089126c3cc68712
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-08-30 16:17:57 -0400

    1

--- src/navigation copy.ts
+++ src/navigation copy.ts
@@ -115,7 +115,7 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'KIKO Stake', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
